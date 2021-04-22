c87bd880fbe0913fd08e7b2828a1fb2dff5f8700
Alexandr <shechenko777777@gmail.com>
2021-04-22 19:28:22 +0300

    page to sauna

--- main.js
+++ main.js
@@ -15,6 +15,24 @@ $(function() {
 		prevArrow: "<img src='accets/images/slidearrowL.svg' class='prev' alt=''>",
     	nextArrow: "<img src='accets/images/slidearrowR.svg' class='next' alt=''>",
 	})
+	 $('.slider-4').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		fade: true,
+		asNavFor: '.slider-5',
+		prevArrow: "<img src='accets/images/slidearrowL.svg' class='prev' alt=''>",
+		nextArrow: "<img src='accets/images/slidearrowR.svg' class='next' alt=''>",
+	});
+	$('.slider-5').slick({
+		centerMode: false,
+		slidesToShow: 7,
+		arrows: false,
+		slidesToScroll: 1,
+		asNavFor: '.slider-4',
+		focusOnSelect: true,
+		infinite: true
+	});
 	$("[data-collapse]").on("click", function(event) {
 		event.preventDefault();
 
